1cecc9359f350fa46db3f5c5df2524b6879d81b0
Jonatan Arce <jonatanarce68@gmail.com>
2022-04-11 22:32:35 -0300

    guardando los archivos

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="navbar-container">
+      <div class="navbar-content">
+        <router-link to="/home"><i class='bx bx-home-alt bx-sm'></i></router-link>
+        <router-link to="/cesta"><i class='bx bx-package bx-sm'></i></router-link>
+        <router-link to ="/deseos"><i class='bx bx-heart bx-sm' ></i></router-link>
+        <router-link to ="/historial"><i class='bx bx-time-five bx-sm' ></i></router-link>
+        </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap');
+
+.navbar-container{
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  .navbar-content{
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+  a{
+  color: rgb(68, 68, 68);
+  &.router-link-exact-active {
+  color: rgb(172, 11, 92);
+}
+    }
+
+  }
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+*{
+  font-family: 'Josefin Sans', sans-serif;
+  margin: 0px;
+  padding: 0px;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="home">
+   <div class="header">
+     <h2 class = "title">Hi <b>Vadim!</b></h2>
+     <div class="input">
+       <input type="text" placeholder="Find what you want...">
+          <div class="icon-input">
+              <i class='bx bx-search'></i>
+          </div>
+        </div>
+      <div class= "combo-header">
+            <div class="text">
+              <h3><b>Free combo</b></h3>
+              <p>Burguer + Coca-Cola for new users</p>
+            </div>
+            <div class="img">
+              <img src="img\icons\hamburguesa.png">
+            </div>
+      </div>
+   </div>
+  </div>
+</template>
+
+<style lang="less">
+
+  .home{
+    margin: 20px;
+  }
+  .header{
+    .combo-header{
+      background-color: rgb(74, 174, 84);
+      color: white;
+      padding: 10px;
+      border-radius: 20px;
+      display:flex;
+      justify-content: space-between;
+      align-items: center;
+      .img{
+        width: 50%;
+        margin-left: auto;
+        img{
+          width: 100%;
+          object-fit: cover;
+          border-radius: 10px;
+          margin-left: 10px;
+        }
+      }
+      .text{
+        width: 50%;
+        h3{
+        font-weight: 400;
+        }
+        p{
+          font-weight: 300;
+        }
+      }
+    }
+    .input{
+      display: flex;
+      width: 100%;
+      align-items: center;
+      margin-top: 12px;
+      position: relative;
+      input{
+        width: 100%;
+        background-color: rgb(248, 248, 248);
+        border: 0ch;
+        outline: none;
+        padding: 10px;
+        padding-left: 30px;
+        border-radius: 10px;
+      }
+      .icon-input{
+        position: absolute;
+        margin-left: 10px;
+        margin-top: 2px;
+      }
+    }
+    .title{
+      font-weight: 350;
+    }
+  }
+</style>
